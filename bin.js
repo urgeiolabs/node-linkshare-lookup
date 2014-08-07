0b4d9bfb0a8d4bd3d48c99492ac41158ee520439
Adam Rudd <adam.rudd@uqconnect.edu.au>
2014-08-07 15:00:04 +0200

    Add bin

---
+++ bin.js
@@ -0,0 +1,43 @@
+/**
+ * Module dependencies
+ */
+var linkshare = require('./')
+  , nomnom = require('nomnom');
+
+var opts = nomnom
+  .script('linkshare-lookup')
+  .nocolors()
+  .option('id', {
+    abbr: 'i',
+    required: true,
+    help: 'Linkshare api token'
+  })
+  .option('keywords', {
+    abbr: 'k',
+    required: true,
+    help: 'Keywords to search'
+  })
+  .option('one', {
+    abbr: '1',
+    help: 'Limit to only one result',
+    flag: true,
+    default: false
+  })
+  .option('limit', {
+    abbr: 'l',
+    help: 'Limit number of results'
+  })
+  .option('page', {
+    abbr: 'p',
+    help: 'Result page',
+  })
+  .parse();
+
+linkshare({keywords: opts.keywords})
+  .id(opts.id)
+  .limit(opts.limit)
+  .one(opts.one)
+  .done(function (err, res) {
+    if (err) throw err;
+    console.log(JSON.stringify(res));
+  });
